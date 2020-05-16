29c3cbf4e93f53f06e9c15235721daf04e2b708d
Ikaa06 <49639996+Ikaa06@users.noreply.github.com>
2020-05-17 02:28:03 +1000

    bot_02

--- handlers/callback_handler.py
+++ handlers/callback_handler.py
@@ -2,7 +2,6 @@ from aiogram.dispatcher import FSMContext
 from config import dp, bot
 
 from keyboards import company_kb_1, company_kb_2, subscribe_kb, unsubscribe_kb
-# import
 
 dp.callback_query_handler
 
@@ -19,8 +18,9 @@ async def callback_inline(call, state: FSMContext):
             await bot.edit_message_reply_markup(inline_message_id=i_m_i, reply_markup=company_kb_1)
 
         elif call.data == 'download_report':
-            # await bot.send_document(call., 'BQACAgIAAxkBAAM7XrbJnnTK2x8EH_up-C_kZXNH1T4AAq8FAAJJhrhJY8REmuWXg1YZBA')
-            pass
+            f = open('7725352740.pdf', 'rb')
+            await bot.send_document(call.message.chat.id, f)
+
 
         elif call.data == 'unsubscribe':
             await bot.edit_message_reply_markup(inline_message_id=i_m_i, reply_markup=subscribe_kb)
--- handlers/commands_handler.py
+++ handlers/commands_handler.py
@@ -1,25 +1,39 @@
 from aiogram import types
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import dp, bot
+from aiogram.dispatcher import FSMContext
 
+
+from config import dp, bot
 from keyboards import subscribe_kb
+from myslq import save_user
+from utils import Admin
 
 
 @dp.message_handler(commands="start")
 async def show_start_message(message: types.Message):
+    try:
+        save_user(message)
+    except:
+        pass
     await message.answer("Привет! "
                          "Я присылаю информацию о российских компаниях и ИП."
                          )
 
+
 @dp.message_handler(commands="help")
 async def show_start_message(message: types.Message):
     await message.answer("Просто отправьте название компании чтобы получить полную информацию о ней\n")
+
 
 @dp.message_handler(commands="settings")
 async def show_start_message(message: types.Message):
     await message.answer("Настройки\n", reply_markup=subscribe_kb)
 
 
+@dp.message_handler(commands=['admin'])
+async def profil(message: types.Message, state: FSMContext):
+    await message.answer('Ваш пароль администратора')
+    await state.update_data(admin='123')
+    await Admin.password.set()
 
 
 
