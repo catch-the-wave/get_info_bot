a12a616f60784dcbffa97cc070fd0ac632a69c61
Ikaa06 <49639996+Ikaa06@users.noreply.github.com>
2020-05-17 04:37:56 +1000

    Add files via upload

--- myslq.py
+++ myslq.py
@@ -11,7 +11,7 @@ conn = persist.connection()
 
 def save_user(message):
     cursor = conn.cursor()
-    add_user = 'INSERT INTO user_id(name, chat_id, sms) VALUES (%(name)s,%(chat_id)s, %(sms)s)'
+    add_user = 'INSERT INTO parents(name, chat_id, sms) VALUES (%(name)s,%(chat_id)s, %(sms)s)'
     # VALUES ('', '55', '454')
     user_id = message.chat.id
     name = message.from_user.first_name
@@ -24,6 +24,8 @@ def save_user(message):
     cursor.execute(add_user, data_person)
     conn.commit()
     cursor.close()
+
+
 def spam_get_date():
     cursor = conn.cursor()
     set = 'SELECT * FROM job_spam WHERE sms="0"'
@@ -31,10 +33,56 @@ def spam_get_date():
     nit = cursor.fetchall()
     cursor.close()
     return nit
+
+
 def rasilkas():
     cursor = conn.cursor()
-    set = 'SELECT chat_id FROM child WHERE sms="0"'
+    set = 'SELECT chat_id FROM parents WHERE sms="0"'
     cursor.execute(set)
     nit = cursor.fetchall()
     cursor.close()
     return nit
+
+
+def smap_not_yes(chat_id):
+    cursor = conn.cursor()
+    set = 'SELECT sms FROM parents WHERE chat_id="{}"'.format(chat_id)
+    cursor.execute(set)
+    nit = cursor.fetchall()
+    cursor.close()
+    return nit[0].get('sms')
+
+
+def smap_up(chat_id, sms=0):
+    cursor = conn.cursor()
+    set = 'UPDATE parents SET sms="{}" WHERE chat_id="{}"'.format(sms, chat_id)
+    cursor.execute(set)
+    conn.commit()
+    cursor.close()
+
+
+def save_favourites(chat_id, first_name, inn):
+    cursor = conn.cursor()
+    set = 'SELECT id FROM parents WHERE chat_id="{}"'.format(chat_id)
+    cursor.execute(set)
+    nit = cursor.fetchall()[0]
+    add_user = 'INSERT INTO favourites(name_id, first_name, inn) VALUES (%(name_id)s,%(first_name)s, %(inn)s)'
+    data_person = {
+        'name_id': nit.get('id'),
+        'first_name': first_name,
+        'inn': inn,
+    }
+    cursor.execute(add_user, data_person)
+    conn.commit()
+    cursor.close()
+
+
+def delete_favourites(chat_id, inn):
+    cursor = conn.cursor()
+    set = 'SELECT id FROM parents WHERE chat_id="{}"'.format(chat_id)
+    cursor.execute(set)
+    nit = cursor.fetchall()[0]
+    set = ' DELETE FROM favourites  WHERE name_id="{}" AND inn="{}"'.format(nit.get('id'), inn)
+    cursor.execute(set)
+    conn.commit()
+    cursor.close()
